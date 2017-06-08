153f6f6107495c420388ece7262a42d0fd8412ce
awalqui <walqang@gmail.com>
2017-06-08 14:44:42 -0400

    Day 4 HW Start

--- README.md
+++ README.md
@@ -12,3 +12,19 @@
 ### Super Mega Bonus Credit Hyper Fighting
 
 * Add buttons to move a flick up and down the list.
+
+## Day 4 Homework
+
+* Fix the issue with flick id values, which sometimes results in removing the wrong flick from the array (and thus from localStorage).
+
+### Bonus Credit
+
+* Also track the year the flick was released.
+
+### Super Mega Bonus Credit
+
+* Edit the names of flicks that are already in the list (and make sure the changes persist across page loads). Gee, it would nice if we could make that span's _content editable_ somehow.
+
+### Super Mega Bonus Credit Hyper Fighting
+
+* Have a good weekend.
--- app.js
+++ app.js
@@ -7,50 +7,55 @@ const app = {
     this.template = document.querySelector(selectors.templateSelector)
     document
       .querySelector(selectors.formSelector)
-      .addEventListener('submit', this.addFlick.bind(this))
+      .addEventListener('submit', this.addFlickViaForm.bind(this))
 
-
+    this.load()
   },
 
-  addFlick(ev) {
+  load() {
+      // Get the JSON string out of localStorage
+      const flicksJSON = localStorage.getItem('flicks')
+
+      // Turn that into an array
+      const flicksArray = JSON.parse(flicksJSON)
+
+      // Set this.flicks to that array
+      if (flicksArray) {
+        flicksArray  
+            .reverse()
+            .map(this.addFlick.bind(this))
+      }
+  },
+
+  addFlick(flick) {
+      const listItem = this.renderListItem(flick)
+      this.list
+        .insertBefore(listItem, this.list.firstChild)
+      
+      ++ this.max
+      this.flicks.unshift(flick)
+      this.save()
+  },
+
+  addFlickViaForm(ev) {
     ev.preventDefault()
     const f = ev.target
     const flick = {
       id: this.max + 1,
       name: f.flickName.value,
     }
-    this.flicks.unshift(flick)
 
-    const listItem = this.renderListItem(flick)
-    this.list.insertBefore(listItem, this.list.firstChild)
+    this.addFlick(flick)
 
-    ++ this.max
     f.reset()
-    //this.save()
   },
-
-  removeFlick(ev) {
-    const g = ev.target
-    const li = g.closes('.flick').remove
-
-    for (let i = 0; i < this.flicks.length; i++) {
-        let inUse = this.flicks[i].id.toString()
-        if (inUse === li.dataset.id) {
-            this.flicks.splice(i, 1)
-            break
-        }
-    }
-
-    li.remove()
-    this.save()
+  
+  save() {
+      localStorage.setItem('flicks', JSON.stringify(this.flicks))
   },
 
   removeClass(element, cName) {
       element.cName = element.cName.replace(cName, '').trim()
-  },
-
-  save() {
-      localStorage.setItem('flick-form', this.save)
   },
   
   renderListItem(flick) {
@@ -61,7 +66,26 @@ const app = {
         .querySelector('.flick-name')
         .textContent = flick.name
 
+    item
+        .querySelector('button.remove')
+        .addEventListener('click', this.removeFlick.bind(this))
+
     return item
+  },
+
+  removeFlick(ev) {
+    const g = ev.target.closest('.flick')
+
+    for (let i = 0; i < this.flicks.length; i++) {
+        const currentId = this.flicks[i].id.toString()
+        if (g.dataset.id === currentId) {
+            this.flicks.splice(i, 1)
+            break
+        }
+    }
+
+    g.remove()
+    this.save() 
   },
 }
 
